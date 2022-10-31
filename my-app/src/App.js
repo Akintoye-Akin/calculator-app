85ea4f5e42d48ff290dabdb3c6849fc0ac623248
Akinladeibraheem <Akinladeibraheem@gmail.com>
2022-10-31 18:50:16 +0100

    lastest commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,82 +1,159 @@
-/* eslint-disable no-eval */
-import "./App.css"
-import {useState } from "react"
-
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import {default as NumberFormat} from "react";
 
 function App() {
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
-  const ops = ["/", "*", "+", "-", "."]
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
 
-  const updateCalc = value =>{   
- if(
-    ops.includes(value) && [calc === "" ||ops.includes(value)] && ops.includes (calc.slice (-1))
- ){ 
-    return; 
- }
+  const inputNum = (e) => {
+    if (curState.includes(".") && e.target.innerText === ".") return;
 
-    setCalc (calc + value)
-    if (!ops.includes (value)){
-      setResult(eval(calc + value).toString())
+    if (total) {
+      setPreState("");
     }
 
-  }
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
+  };
 
+  useEffect(() => {
+    setInput(curState);
+  }, [curState]);
 
+  useEffect(() => {
+    setInput("0");
+  }, []);
+  const operatorType = (e) => {
+    setTotal(false);
+    setOperator(e.target.innerText);
+    if (curState === "") return;
+    if (preState !== "") {
+      equals();
+    } else {
+      setPreState(curState);
+      setCurState("");
+    }
+  };
 
-let createDigits = ( ) => {
-let  digits = [];
-for (let i = 1; i < 10; i++ ){
-  digits.push(  
-<button 
-onClick={( )=>{ updateCalc (i.toString())}} 
+  const equals = (e) => {
+    if (e?.target.innerText === "=") {
+      setTotal(true);
+    }
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
 
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case "X":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+      default:
+        return;
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
+  };
 
-key= {i}> {i}</button>
+  const minusPlus = () => {
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-" + curState);
+    }
+  };
 
-  )
-}
-return digits;
+  const percent = () => {
+    preState
+      ? setCurState(String((parseFloat(curState) / 100) * preState))
+      : setCurState(String(parseFloat(curState) / 100));
+  };
 
-}
-const calculate = () => { setCalc (eval(calc).toString())}
-
-const deleteLast = () =>{
-  if (calc === ""){
-      return;
-  } const value = calc.slice (0, -1)
-  setCalc (value)
-};
-
-
-
-
+  const reset = () => {
+    setPreState("");
+    setCurState("");
+    setInput("0");
+  };
   return (
-    <div className="container">
-      <div className="calculator">
-        <div className="display">
-          { result ?  <span> ({result}) </span> : " " }
-          
-          {calc || "0" }
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='screen'>
+          {input !== "" || input === "0" ? (
+            <NumberFormat value={input} displayType={"text"} thousandSeparator={true} />
+          ) : (
+            <NumberFormat value={preState} displayType={"text"} thousandSeparator={true} />
+          )}
         </div>
-        <div className="operators">
-          <button onClick={ () => updateCalc("/") }>/ </button>
-          <button  onClick={( )=>{ updateCalc ("+")}}  >+</button>
-          <button onClick={( )=>{ updateCalc ("-")}}  >-</button>
-          <button onClick={( )=>{ updateCalc ("*")}}  >*</button>
-
-
-          <button onClick={deleteLast}> DEL </button>
+        <div className='btn coral' onClick={reset}>
+          AC
         </div>
-
-        <button> DEL </button>
-        <div className="digits">
-          {createDigits()}
-          <button onClick={( )=>{ updateCalc ("0")}}  > 0 </button>
-          <button onClick={( )=>{ updateCalc (".   ")}} > . </button>
-          <button onClick={  calculate} > = </button>
-          </div>
+        <div className='btn coral' onClick={percent}>
+          %
+        </div>
+        <div className='btn coral' onClick={minusPlus}>
+          +/-
+        </div>
+        <div className='btn red' onClick={operatorType}>
+          /
+        </div>
+        <div className='btn' onClick={inputNum}>
+          7
+        </div>
+        <div className='btn' onClick={inputNum}>
+          8
+        </div>
+        <div className='btn' onClick={inputNum}>
+          9
+        </div>
+        <div className='btn red' onClick={operatorType}>
+          X
+        </div>
+        <div className='btn' onClick={inputNum}>
+          4
+        </div>
+        <div className='btn' onClick={inputNum}>
+          5
+        </div>
+        <div className='btn' onClick={inputNum}>
+          6
+        </div>
+        <div className='btn red' onClick={operatorType}>
+          +
+        </div>
+        <div className='btn' onClick={inputNum}>
+          1
+        </div>
+        <div className='btn' onClick={inputNum}>
+          2
+        </div>
+        <div className='btn' onClick={inputNum}>
+          3
+        </div>
+        <div className='btn red' onClick={operatorType}>
+          -
+        </div>
+        <div className='btn zero' onClick={inputNum}>
+          0
+        </div>
+        <div className='btn' onClick={inputNum}>
+          .
+        </div>
+        <div className='btn' onClick={equals}>
+          =
+        </div>
       </div>
     </div>
   );
